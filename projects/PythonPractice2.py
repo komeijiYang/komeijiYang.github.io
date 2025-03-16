9687af14ed2a3e2e7b212353f8e7b0e068d7fbc9
komeijiYang <1084441602@qq.com>
2025-03-16 20:32:45 +0800

    Add files via upload

---
+++ projects/PythonPractice2.py
@@ -0,0 +1,31 @@
+﻿#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+def mul(x, *y):
+    rst=x
+    if y==None:
+        return rst
+    else:
+        for n in y:
+            rst*=n
+        return rst
+
+# 测试
+print('mul(5) =', mul(5))
+print('mul(5, 6) =', mul(5, 6))
+print('mul(5, 6, 7) =', mul(5, 6, 7))
+print('mul(5, 6, 7, 9) =', mul(5, 6, 7, 9))
+if mul(5) != 5:
+    print('mul(5)测试失败!')
+elif mul(5, 6) != 30:
+    print('mul(5, 6)测试失败!')
+elif mul(5, 6, 7) != 210:
+    print('mul(5, 6, 7)测试失败!')
+elif mul(5, 6, 7, 9) != 1890:
+    print('mul(5, 6, 7, 9)测试失败!')
+else:
+    try:
+        mul()
+        print('mul()测试失败!')
+    except TypeError:
+        print('测试成功!')
---
+++ projects/PythonPractice3.py
@@ -0,0 +1,107 @@
+﻿#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+#汉诺塔问题
+def move(n, a, b, c):
+    if n == 1:
+        print(a, '-->', c)
+        pass
+    else:
+        move(n-1, a, c, b)
+        move(1, a, b, c)
+        move(n-1, b, a, c)
+        pass
+move(3, 'A', 'B', 'C')   
+
+def trim(s):
+    if not s:
+        return s
+    while s[:1]==' ':
+        s=s[1:]
+    while s[-1:]==' ':
+        s=s[:-1]
+    return s
+
+# 测试:
+if trim('hello  ') != 'hello':
+    print('测试失败!')
+elif trim('  hello') != 'hello':
+    print('测试失败!')
+elif trim('  hello  ') != 'hello':
+    print('测试失败!')
+elif trim('  hello  world  ') != 'hello  world':
+    print('测试失败!')
+elif trim('') != '':
+    print('测试失败!')
+elif trim('    ') != '':
+    print('测试失败!')
+else:
+    print('测试成功!')   
+
+def findMinAndMax(L):
+    if L==[]:
+        return (None, None)
+    else:
+        max_val=L[0]
+        min_val=L[0]
+        for x in L:
+            if x>max_val:
+                max_val=x
+            if x<min_val:
+                min_val=x
+    return (min_val,max_val)
+
+# 测试
+if findMinAndMax([]) != (None, None):
+    print('测试失败!')
+elif findMinAndMax([7]) != (7, 7):
+    print('测试失败!')
+elif findMinAndMax([7, 1]) != (1, 7):
+    print('测试失败!')
+elif findMinAndMax([7, 1, 3, 9, 5]) != (1, 9):
+    print('测试失、败!')
+else:
+    print('测试成功!')
+
+#杨辉三角
+def triangles():
+    row=[1]
+    while True:
+        yield row
+        row=[i+j for i,j in zip(row+[0],[0]+row)]
+# 期待输出:x
+# [1]
+# [1, 1]
+# [1, 2, 1]
+# [1, 3, 3, 1]
+# [1, 4, 6, 4, 1]
+# [1, 5, 10, 10, 5, 1]
+# [1, 6, 15, 20, 15, 6, 1]
+# [1, 7, 21, 35, 35, 21, 7, 1]
+# [1, 8, 28, 56, 70, 56, 28, 8, 1]
+# [1, 9, 36, 84, 126, 126, 84, 36, 9, 1]
+n = 0
+results = []
+for t in triangles():
+    results.append(t)
+    n = n + 1
+    if n == 10:
+        break
+
+for t in results:
+    print(t)
+
+if results == [
+    [1],
+    [1, 1],
+    [1, 2, 1],
+    [1, 3, 3, 1],
+    [1, 4, 6, 4, 1],
+    [1, 5, 10, 10, 5, 1],
+    [1, 6, 15, 20, 15, 6, 1],
+    [1, 7, 21, 35, 35, 21, 7, 1],
+    [1, 8, 28, 56, 70, 56, 28, 8, 1],
+    [1, 9, 36, 84, 126, 126, 84, 36, 9, 1]
+]:
+    print('测试通过!')
+else:
+    print('测试失败!')
